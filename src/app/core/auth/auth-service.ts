f82046d4347852b8e30dccd27232073a08368b1e
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2026-02-13 00:42:32 +0100

    Realiser la logique de connection et register des utilisateurs

---
+++ src/app/core/auth/auth-service.ts
@@ -0,0 +1,36 @@
+import { inject, Injectable } from '@angular/core';
+import { RegisterRequest, User } from '../models/user.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment.development';
+import { map, Observable, switchMap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+    private readonly http = inject(HttpClient);
+    private apiUrl = `${environment.jsonServerUrl}/users`;
+
+    register(registerData: RegisterRequest): Observable<User | null> {
+        return this.http.get<User[]>(`${this.apiUrl}?email=${registerData.email}`).pipe(
+            switchMap(users => {
+                if (users.length > 0) {
+                    throw new Error('This email is already registered!');
+                }
+            
+            return this.http.post<User>(this.apiUrl, registerData);
+            })
+        );
+    }
+
+    login(email: string, password: string): Observable<User | null> {
+        return this.http.get<User[]>(`${this.apiUrl}?email=${email}&password=${password}`).pipe(
+            map(users => {
+                if (users.length > 0) {
+                    return users[0];
+                }
+                return null;
+            })
+        );
+    }
+}
--- src/app/store/auth.store.ts
+++ src/app/store/auth.store.ts
@@ -1,8 +1,13 @@
 import { computed, inject } from "@angular/core";
-import { User } from "../core/models/user.model";
+import { RegisterRequest, User } from "../core/models/user.model";
 import { patchState, signalStore, withComputed, withHooks, withMethods, withState } from "@ngrx/signals";
 import { environment } from "../../environments/environment.development";
 import { CookieService } from "ngx-cookie-service";
+import { Router } from "@angular/router";
+import { AuthService } from "../core/auth/auth-service";
+import { error } from "console";
+import { toast } from "ngx-sonner";
+import { register } from "module";
 
 interface AuthState {
     user: User | null,
@@ -27,18 +32,66 @@ export const AuthStore = signalStore(
         userRole: computed(() => !!(store.user()) ? 'client' : 'visitor'),
     })),
 
-    withMethods((store, cookieService = inject(CookieService)) => ({
+    withMethods((
+        store, 
+        cookieService = inject(CookieService),
+        router = inject(Router),
+        authService = inject(AuthService),
+    ) => ({
         setLoading(value: boolean) {
             patchState(store, { isLoading: value })
         },
 
+        login(email: string, password: string) {
+            this.setLoading(true);
+
+            authService.login(email, password).subscribe({
+                next: (user) => {
+                    if (user) {
+                        this.setLogin(user);
+                        toast.success(`Welcome ${user.firstName}`);
+                        router.navigate(['/']);
+                    } else {
+                        this.setLoading(false);
+                        toast.error('Authentication Failed', {
+                            description: 'Invalid email or password'
+                        });
+                    }
+                },
+                error: (error) => {
+                    this.setLoading(false);
+
+                    toast.error('Server Error', {
+                        description: error.message
+                    });
+                    }
+            })
+        },
+
+        register(registerData: RegisterRequest) {
+            this.setLoading(true);
+
+            authService.register(registerData).subscribe({
+                next: () => {
+                    this.setLoading(false);
+                    toast.success('Account Created', {
+                        description: 'You can now log in to your account'
+                    });
+                    router.navigate(['/login']);
+                },
+                error: (error) => {
+                    this.setLoading(false);
+                    toast.warning('Registration Issue', {
+                        description: error.message
+                    });
+                }
+            });
+        },
+
+
         setLogin(user: User) {
-            patchState(store, { isLoading: true })
-
             const isSecure = environment.secureCookie;
-
             cookieService.set('user', JSON.stringify(user), 7, '/', '', isSecure, 'Strict');
-
             patchState(store, { user, isLoading: false });
         },
 
