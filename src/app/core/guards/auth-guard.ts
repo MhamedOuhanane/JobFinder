f560d88f18e242bfa591a621f285280559a78434
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2026-02-12 23:58:04 +0100

    Realiser la logique des guardes authGuarde et roleGuarde

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -1,5 +1,19 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthStore } from '../../store/auth.store';
+import { toast } from 'ngx-sonner';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+    const authStore = inject(AuthStore);
+    const router = inject(Router);
+
+    if (authStore.isAuthenticated()) {
+        return true;
+    }
+
+    toast.error('Access Denied', {
+            description: 'Please login to access this page.',
+    });
+    
+    return router.createUrlTree(['/login']);
 };
--- src/app/core/guards/role-guard.ts
+++ src/app/core/guards/role-guard.ts
@@ -1,5 +1,23 @@
 import { CanActivateFn } from '@angular/router';
+import { AuthStore } from '../../store/auth.store';
+import { inject } from '@angular/core';
+import { Router } from 'express';
+import { toast } from 'ngx-sonner';
 
 export const roleGuard: CanActivateFn = (route, state) => {
-  return true;
+    const authStore = inject(AuthStore);
+    const router = inject(Router);
+
+    const userRole = authStore.userRole();
+    const expectedRole = route.data['roles'] as Array<string>;
+
+    if (expectedRole.includes(userRole!)) {
+        return true;
+    }
+
+    toast.error('Permission Denied', {
+        description: "You don't have the required permissions for this area.",
+    });
+
+    return router.createUrlTree(['/unauthorized']);
 };
